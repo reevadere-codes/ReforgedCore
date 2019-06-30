9bfacbdaa1fd61fbc88490443839c1ed7f22d289
HIPPOKLEIDES <pawilkinson20@gmail.com>
2019-06-30 14:57:35 -0700

    use quartz_block as parent for cube for top/bottom texture

--- src/main/java/com/conquestreforged/core/block/standard/Cube.java
+++ src/main/java/com/conquestreforged/core/block/standard/Cube.java
@@ -6,9 +6,9 @@ import com.conquestreforged.core.asset.annotation.State;
 import net.minecraft.block.Block;
 
 @Assets(
-        state = @State(name = "%s", template = "acacia_planks", plural = true),
-        item = @Model(name = "item/%s", parent = "block/%s", template = "item/acacia_planks", plural = true),
-        block = @Model(name = "block/%s", template = "block/acacia_planks", plural = true)
+        state = @State(name = "%s", template = "quartz_block"),
+        item = @Model(name = "item/%s", parent = "block/%s", template = "item/quartz_block"),
+        block = @Model(name = "block/%s", template = "block/quartz_block")
 )
 public class Cube extends Block {
     public Cube(Properties properties) {
