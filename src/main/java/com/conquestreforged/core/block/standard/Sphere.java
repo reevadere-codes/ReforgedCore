aaef20e0ebe6708769b505f094d6f779c153e364
HIPPOKLEIDES <pawilkinson20@gmail.com>
2019-06-30 14:42:28 -0700

    fixes for RoundArch, RoundArchHalf, and Sphere classes

--- src/main/java/com/conquestreforged/core/block/standard/Sphere.java
+++ src/main/java/com/conquestreforged/core/block/standard/Sphere.java
@@ -3,82 +3,49 @@ package com.conquestreforged.core.block.standard;
 import com.conquestreforged.core.asset.annotation.Assets;
 import com.conquestreforged.core.asset.annotation.Model;
 import com.conquestreforged.core.asset.annotation.State;
+import com.conquestreforged.core.block.base.WaterloggedShape;
+import com.conquestreforged.core.block.properties.SphereShape;
 import com.conquestreforged.core.block.properties.Waterloggable;
-import com.conquestreforged.core.block.properties.CapitalDirection;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.fluid.Fluids;
-import net.minecraft.fluid.IFluidState;
-import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.EnumProperty;
 import net.minecraft.state.StateContainer;
-import net.minecraft.state.properties.Half;
-import net.minecraft.util.Direction;
+import net.minecraft.util.math.shapes.VoxelShape;
+
 
 @Assets(
         state = @State(name = "%s_sphere", template = "parent_sphere"),
-        item = @Model(name = "item/%s_sphere", parent = "block/%s_sphere", template = "item/parent_sphere"),
+        item = @Model(name = "item/%s_sphere", parent = "block/%s_sphere", template = "item/dragon_egg"),
         block = {
                 @Model(name = "block/%s_sphere", template = "block/parent_sphere"),
-                @Model(name = "block/%s_sphere_dragonegg", template = "block/parent_sphere"),
-                @Model(name = "block/%s_sphere_small", template = "block/parent_sphere"),
+                @Model(name = "block/%s_sphere_dragonegg", template = "block/dragon_egg"),
+                @Model(name = "block/%s_sphere_small", template = "block/parent_sphere_small"),
         }
 )
-public class Sphere extends Block implements Waterloggable {
+public class Sphere extends WaterloggedShape implements Waterloggable {
 
-    public static final EnumProperty<CapitalDirection> FACING = EnumProperty.create("facing", CapitalDirection.class);
-    public static final EnumProperty<Half> TYPE = EnumProperty.create("type", Half.class);
+    private static final VoxelShape SHAPE_SMALL = Block.makeCuboidShape(3.0D, 0.0D, 3.0D, 13.0D, 11.0D, 13.0D);
+    protected static final VoxelShape SHAPE = Block.makeCuboidShape(1.0D, 0.0D, 1.0D, 15.0D, 16.0D, 15.0D);
 
-    public Sphere(Properties properties) {
+    public static final EnumProperty TYPE = EnumProperty.create("type", SphereShape.class);
+
+    public Sphere(Block.Properties properties) {
         super(properties);
-        setDefaultState((stateContainer.getBaseState()).with(TYPE, Half.TOP).with(FACING, CapitalDirection.NORTH).with(WATERLOGGED, false));
+        setDefaultState((stateContainer.getBaseState()).with(TYPE, SphereShape.LARGE).with(WATERLOGGED, false));
 
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state) {
+        if (state.get(TYPE) == SphereShape.SMALL) {
+            return SHAPE_SMALL;
+        } else {
+            return SHAPE;
+        }
     }
 
     @Override
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-        builder.add(TYPE, FACING, WATERLOGGED);
-    }
-
-    @Override
-    public BlockState getStateForPlacement(BlockItemUseContext context) {
-        IFluidState ifluidstate = context.getWorld().getFluidState(context.getPos());
-        Direction facing = context.getFace();
-
-        CapitalDirection horizontalFacing;
-        switch (facing) {
-            default:
-                horizontalFacing = CapitalDirection.FLAT;
-                break;
-            case NORTH:
-                horizontalFacing = CapitalDirection.SOUTH;
-                break;
-            case SOUTH:
-                horizontalFacing = CapitalDirection.NORTH;
-                break;
-            case WEST:
-                horizontalFacing = CapitalDirection.EAST;
-                break;
-            case EAST:
-                horizontalFacing = CapitalDirection.WEST;
-                break;
-        }
-
-        Half verticalFacing;
-        if (facing != Direction.DOWN && (facing == Direction.UP || context.func_221532_j().y <= 0.5D)) {
-            verticalFacing = Half.BOTTOM;
-        } else {
-            verticalFacing = Half.TOP;
-        }
-
-        return super.getStateForPlacement(context)
-                .with(TYPE, verticalFacing)
-                .with(FACING, horizontalFacing)
-                .with(WATERLOGGED, ifluidstate.getFluid() == Fluids.WATER);
-    }
-
-    @Override
-    public IFluidState getFluidState(BlockState state) {
-        return Waterloggable.getFluidState(state);
+        builder.add(TYPE, WATERLOGGED);
     }
 }
