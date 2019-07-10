5a79b3b512243b9eb8a20246a881d6542dd34a9e
HIPPOKLEIDES <pawilkinson20@gmail.com>
2019-07-09 19:01:21 -0700

    plural for cube

--- src/main/java/com/conquestreforged/core/block/standard/Cube.java
+++ src/main/java/com/conquestreforged/core/block/standard/Cube.java
@@ -6,9 +6,9 @@ import com.conquestreforged.core.asset.annotation.State;
 import net.minecraft.block.Block;
 
 @Assets(
-        state = @State(name = "%s", template = "parent_cube"),
-        item = @Model(name = "item/%s", parent = "block/%s", template = "item/parent_cube"),
-        block = @Model(name = "block/%s", template = "block/parent_cube")
+        state = @State(name = "%s", template = "parent_cube", plural = true),
+        item = @Model(name = "item/%s", parent = "block/%s", template = "item/parent_cube", plural = true),
+        block = @Model(name = "block/%s", template = "block/parent_cube", plural = true)
 )
 public class Cube extends Block {
     public Cube(Properties properties) {
